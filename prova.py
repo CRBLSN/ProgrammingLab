5b93a23932bdbb8589c16ef3dd87401c57a46c85
CRBLSN <antale0105@gmail.com>
2025-03-19 18:34:37 +0100

    aggiunge modifiche di speranze su  git nel file prova

--- prova.py
+++ prova.py
@@ -1,2 +1,10 @@
-print("hello worlds")
+'''questo è un test per guardare se git 
+   mi chiede le credenziali ad ogni push 
+   o meno'''
+
+speranza = "speriamo che git non me le chieda ogni volta"
+speranza=speranza.split(" ")
+for parola in speranza:
+    print(parola)
+
 
